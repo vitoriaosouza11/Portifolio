f5b64945e73be03ef95669186c21525b983dd9fc
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2026-04-09 14:08:24 -0300

    Colocando Animações

---
+++ script.js
@@ -0,0 +1,19 @@
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+// CURSOR
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", e => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
